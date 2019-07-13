9b286bebb6eaaaf31a4d6f66cecc2c916ca79335
muhammadzhuhry <muhammadzhuhry@gmail.com>
2019-07-13 19:28:38 +0700

    create basic server

---
+++ index.js
@@ -0,0 +1,71 @@
+const moment = require('moment');
+const uuidv4 = require('uuid/v4');
+const restify = require('restify');
+const mongoose = require('mongoose');
+const project = require('./package.json');
+
+const port = 9000;
+const  time = moment().format('DD/MM/YYYY hh:mm:ss a');
+
+// import model
+require('./models/publisher.model');
+const Publisher = mongoose.model('publisher');
+
+// create an server
+const app = restify.createServer({
+  name: `${project.name}-server`,
+  version: project.version
+});
+
+app.use(restify.plugins.bodyParser());
+app.use(restify.plugins.queryParser());
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/test-books', {
+  useNewUrlParser: true
+})
+  .then(() => console.log(`MongoDB connected...`))
+  .catch(err => console.log(err));
+
+// root
+app.get('/', (req, res) => {
+  res.send(`{ success:true, data:Index, message:${project.name}, code:200 }`)
+});
+
+// endpoint publisher
+app.post('/publisher', (req, res) => {
+  let payload = {
+    publisher_id: uuidv4(),
+    publisher_name: req.body.publisher_name,
+    city: req.body.city
+  };
+
+  Publisher.create(payload, (err, value) => {
+    if (err) {
+      return res.status(500).send({'error':'An error has occurred'});
+    }
+
+    res.send({
+      'code': 201,
+      'success': 'true',
+      'message': 'Publisher has been inserted',
+      'data': value
+    });
+  });
+});
+
+app.get('/publisher', (req, res) => {
+  Publisher.find({})
+    .then((value) => {
+      res.send({
+        'code': 200,
+        'success': 'true',
+        'message': 'Request has been proceseed',
+        'data': value
+      });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port} on ${time}`);
+});
