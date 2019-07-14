1716083d7e42fb38f9b050bc6c4ab25d597710e8
muhammadzhuhry <muhammadzhuhry@gmail.com>
2019-07-14 15:21:36 +0700

    add endpoint author

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const restify = require('restify');
 const project = require('./package.json');
+const authorController = require('./controllers/author.controller');
 const publisherController = require('./controllers/publisher.controller');
 
 function AppServer() {
@@ -16,6 +17,12 @@ function AppServer() {
   this.server.get('/', (req, res) => {
     res.send({ success:true, data:'index', message:'This service is running properly', code:200 });
   });
+
+  // author route
+  this.server.get('/author', authorController.getHandler);
+  this.server.post('/author', authorController.postHandler);
+  this.server.put('/author/:id', authorController.putHandler);
+  this.server.del('/author/:id', authorController.deleteHandler);
 
   // publisher route
   this.server.get('/publisher', publisherController.getHandler);
